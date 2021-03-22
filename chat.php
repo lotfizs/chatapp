56d8c6c4ab3fd422072f4d448d7e340e3862ea62
lotfizs <2133349@talnet.nl>
2021-03-22 16:40:53 +0100

    web

---
+++ chat.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>LZS Chat App</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
+</head>
+<body>
+<div class="wrapper">
+    <section class="chat-area">
+        <header>
+            <a href="#" class="back-icon"><i class="fas fa-arrow-left"></i></a>
+            <img src="img/img.jpg" alt="">
+            <div class="details">
+                <span>Lotfi zs</span>
+                <p>Active now</p>
+            </div>
+        </header>
+    <div class="chat-box">
+        <div class="chat outgoing">
+            <div class="details">
+                <p>Lorem ipsum dolor sit amet, consecteur adipisicing elit.</p>
+            </div>
+        </div>
+        <div class="chat incoming">
+            <img src="img/img.jpg" alt="">
+            <div class="details">
+                <p>Lorem ipsum dolor sit amet, consecteur adipisicing elit.</p>
+            </div>
+        </div>
+        <div class="chat outgoing">
+            <div class="details">
+                <p>Lorem ipsum dolor sit amet, consecteur adipisicing elit.</p>
+            </div>
+        </div>
+        <div class="chat incoming">
+            <img src="img/img.jpg" alt="">
+            <div class="details">
+                <p>Lorem ipsum dolor sit amet, consecteur adipisicing elit.</p>
+            </div>
+        </div>
+        <div class="chat outgoing">
+            <div class="details">
+                <p>Lorem ipsum dolor sit amet, consecteur adipisicing elit.</p>
+            </div>
+        </div>
+        <div class="chat incoming">
+            <img src="img/img.jpg" alt="">
+            <div class="details">
+                <p>Lorem ipsum dolor sit amet, consecteur adipisicing elit.</p>
+            </div>
+        </div>
+        <div class="chat outgoing">
+            <div class="details">
+                <p>Lorem ipsum dolor sit amet, consecteur adipisicing elit.</p>
+            </div>
+        </div>
+        <div class="chat incoming">
+            <img src="img/img.jpg" alt="">
+            <div class="details">
+                <p>Lorem ipsum dolor sit amet, consecteur adipisicing elit.</p>
+            </div>
+        </div>
+    </div>
+        <form action="#" class="typing-area">
+            <input type="text" placeholder="Type a message here...">
+            <button><i class="fab fa-telegram-plane"></i></button>
+        </form>
+    </section>
+</div>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>LZS Chat App</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
+</head>
+<body>
+  <div class="wrapper">
+      <section class="form signup">
+          <header>LZS Chat App</header>
+          <form action="#" enctype="multipart/form-data">
+              <div class="error-txt"></div>
+              <div class="name-details">
+                  <div class="field input">
+                      <label>First Name</label>
+                      <input type="text" name="fname" placeholder="First Name" required>
+                  </div>
+                  <div class="field input">
+                      <label>Last Name</label>
+                      <input type="text" name="lname" placeholder="Last Name" required>
+                  </div>
+              </div>
+              <div class="field input">
+                  <label>Email adress</label>
+                  <input type="text" name="email" placeholder="Emter your email" required>
+              </div>
+              <div class="field input">
+                  <label>Password</label>
+                  <input type="password" name="password" placeholder="Enter new password" required>
+                  <i class="fas fa-eye"></i>
+              </div>
+              <div class="field image">
+                  <label>Select Image</label>
+                  <input type="file" name="image" >
+              </div>
+              <div class="field button">
+                  <input type="submit" value="Continue to Chat">
+              </div>
+          </form>
+          <div class="link">Already signed up? <a href="#">Login now</a></div>
+      </section>
+  </div>
+
+  <script src="JavaScript/pass-show-hide.js"></script>
+  <script src="JavaScript/signup.js"></script>
+
+</body>
+</html>
+jjj
+lll
